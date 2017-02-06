f8c5a16866db37d831ccb4690ff548d8deaf0f8c
Kai Toedter <kai.toedter@siemens.com>
2017-02-06 13:29:52 +0100

    - spring boot 1.4.4.RELEASE

--- subprojects/resource-server/src/main/java/org/eclipse/sw360/rest/resourceserver/release/Sw360ReleaseService.java
+++ subprojects/resource-server/src/main/java/org/eclipse/sw360/rest/resourceserver/release/Sw360ReleaseService.java
@@ -1,5 +1,5 @@
 /*
- * Copyright Siemens AG, 2016. Part of the SW360 Portal Release.
+ * Copyright Siemens AG, 2017. Part of the SW360 Portal Release.
  *
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
